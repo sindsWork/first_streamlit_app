65ed71540d3e33338f2f980671d20613f83acbef
sindsWork <95308195+sindsWork@users.noreply.github.com>
2024-01-15 13:43:05 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,6 @@ streamlit.text('oatmeal')
 streamlit.text('kale spinach smoothie')
 streamlit.text('hard boiled egg')
 
-my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt)
+my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
                               streamlit.dataframe(my_fruit_list)
                               
