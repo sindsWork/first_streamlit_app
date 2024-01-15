cf675794692427e5cc933f4105f5ec0c390b97c4
sindsWork <95308195+sindsWork@users.noreply.github.com>
2024-01-15 15:21:07 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ streamlit.text('hard boiled egg')
 
 my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list=my_fruit_list.set_index('Fruit')
-streamlit.multiselect("Pick some fruits",ist(my_fruit_list.index),['Avovado','Strawberries'])
+streamlit.multiselect("Pick some fruits",list(my_fruit_list.index),['Avovado','Strawberries'])
 streamlit.dataframe(my_fruit_list)
 
 
