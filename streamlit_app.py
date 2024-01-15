f114de74451061170fd91126404cd26202e771c1
sindsWork <95308195+sindsWork@users.noreply.github.com>
2024-01-15 13:42:24 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,11 @@
 import streamlit
+import pandas
 streamlit.title('My parents new healthy Diners')
 streamlit.header('Breakfast Menu')
 streamlit.text('oatmeal')
 streamlit.text('kale spinach smoothie')
 streamlit.text('hard boiled egg')
+
+my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt)
+                              streamlit.dataframe(my_fruit_list)
+                              
