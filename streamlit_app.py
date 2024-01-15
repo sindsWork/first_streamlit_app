b1cb272fbe27892f13573ba2f17ee408fea44676
sindsWork <95308195+sindsWork@users.noreply.github.com>
2024-01-15 13:11:48 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('My parents new healthy Diners')
+streamlit.header('Breakfast Menu')
+streamlit.text('oatmeal')
+streamlit.text('kale spinach smoothie')
+streamlit.text('hard boiled egg')
