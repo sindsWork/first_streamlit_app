e0418defb25572e8b723864e92a4bec6a91c1d16
sindsWork <95308195+sindsWork@users.noreply.github.com>
2024-01-16 13:28:21 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,27 +1,36 @@
 import streamlit
 import pandas
-
-streamlit.title('My parents new healthy Diners')
-streamlit.header('Breakfast Menu')
-streamlit.text('oatmeal')
-streamlit.text('kale spinach smoothie')
-streamlit.text('hard boiled egg')
-
-my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list=my_fruit_list.set_index('Fruit')
-fruit_selected=streamlit.multiselect("Pick some fruits",list(my_fruit_list.index),['Avocado','Strawberries'])
-fruit_to_show=my_fruit_list.loc[fruit_selected]
-streamlit.dataframe(fruit_to_show)
-streamlit.header("Fruityvice Fruit Advice!")
 import requests
-
-streamlit.text(fruityvice_response)
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit" + "kiwi")
-
-fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
-streamlit.dataframe(fruityvice_normalized)
 import snowflake.connector
-
-
-
-                              
+ 
+streamlit.title('My Parents New Healthy Diner')
+ 
+streamlit.header('Breakfast Menu')
+streamlit.text(' 🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free_Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
+ 
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+ 
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
+ 
+# Let's put a pick list here so they can pick the fruit they want to include 
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+ 
+# Display the table on the page.
+streamlit.dataframe(fruits_to_show)
+ 
+#New Sction to display fruityvice api response
+streamlit.header("Fruityvice Fruit Advice!")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+ 
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+ 
+# write your own comment -what does the next line do? 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# write your own comment - what does this do?
+streamlit.dataframe(fruityvice_normalized)
