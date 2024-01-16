2abe71dfa3e4219c364a6fb0e27831e2dded3f69
sindsWork <95308195+sindsWork@users.noreply.github.com>
2024-01-16 06:43:01 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,6 +17,9 @@ import requests
 
 streamlit.text(fruityvice_response)
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response.json())
+fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
+streamlit.dataframe(fruityvice_normalized)
 
 
 
